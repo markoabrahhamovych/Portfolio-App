2ea78cb8c721902e0ae4734d761112f408db8f2b
M.Abrahamovych <marko.abrahamovych@probegin.com>
2024-09-19 15:43:12 +0300

    Implemented animation switching page

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,96 @@
   \***********************/
 /***/ (() => {
 
+//turn pages click next or prev button
 
+var pageTurnBtn = document.querySelectorAll(".next-prev-btn");
+(pageTurnBtn || []).forEach(function (el, index) {
+  el.onclick = function () {
+    var pageTurnId = el.getAttribute("data-page");
+    var pageTurn = document.getElementById(pageTurnId);
+    if (pageTurn.classList.contains("turn")) {
+      pageTurn.classList.remove("turn");
+      setTimeout(function () {
+        pageTurn.style.zIndex = 20 - index;
+      }, 200);
+    } else {
+      pageTurn.classList.add("turn");
+      setTimeout(function () {
+        pageTurn.style.zIndex = 20 + index;
+      }, 200);
+    }
+  };
+});
+
+// contact me button when click
+
+var pages = document.querySelectorAll(".book-page.page-right");
+var contactMeBtn = document.querySelector(".btn.contact-me");
+contactMeBtn.onclick = function () {
+  (pages || []).forEach(function (page, index) {
+    setTimeout(function () {
+      page.classList.add("turn");
+      setTimeout(function () {
+        page.style.zIndex = 20 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+// create reverse index function
+
+var totalPages = pages.length;
+var pageNumber = 0;
+function reverseIndex() {
+  pageNumber--;
+  if (pageNumber < 0) {
+    pageNumber = totalPages - 1;
+  }
+}
+
+//back profile button when click
+
+var backProfileBtn = document.querySelector(".back-profile");
+backProfileBtn.onclick = function () {
+  pages.forEach(function (e, index) {
+    setTimeout(function () {
+      reverseIndex();
+      pages[pageNumber].classList.remove("turn");
+      setTimeout(function () {
+        pages[pageNumber].style.zIndex = 10 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+//opening animation
+var coverRight = document.querySelector(".cover.cover-right");
+var pageLeft = document.querySelector(".book-page.page-left");
+
+//opening animation (cover right animation)
+setTimeout(function () {
+  coverRight.classList.add("turn");
+}, 2100);
+setTimeout(function () {
+  coverRight.style.zIndex = -1;
+}, 2800);
+
+// opening animation (page left or profile page animation)
+setTimeout(function () {
+  pageLeft.style.zIndex = 20;
+}, 3200);
+
+// opening animation (all page right animation)
+
+(pages || []).forEach(function (e, index) {
+  setTimeout(function () {
+    reverseIndex();
+    pages[pageNumber].classList.remove("turn");
+    setTimeout(function () {
+      pages[pageNumber].style.zIndex = 10 + index;
+    }, 500);
+  }, (index + 1) * 200 + 2100);
+});
 
 /***/ }),
 
